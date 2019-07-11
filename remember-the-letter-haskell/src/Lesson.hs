08796c81f9041eea616d8b68229f18a96bbd38b5
Elsanussi Mneina <elsanussimneina@Elsanussis-MacBook-Pro.local>
2019-07-11 16:49:59 -0400

    Refactor: break lessonSummary into two constants.

--- remember-the-letter-haskell/src/Lesson.hs
+++ remember-the-letter-haskell/src/Lesson.hs
@@ -1,4 +1,12 @@
-module Lesson where
+module Lesson
+    ( lessonSummary
+    ) where
 
 lessonSummary :: String
-lessonSummary =  "the | le/la \n a | un/une\n"
+lessonSummary =  flashcard1 ++ " \n " ++ flashcard2 ++ " \n "
+
+flashcard1 :: String
+flashcard1 = "the | le/la"
+
+flashcard2 :: String
+flashcard2 = "a | un/une"
