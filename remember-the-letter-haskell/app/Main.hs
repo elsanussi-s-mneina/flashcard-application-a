edee508137e4b24d7a295d54a406b288022e8a42
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2019-07-17 00:25:51 -0400

    A more consistent prompt.
    Put the prompt character on a new line. It looks better.

--- remember-the-letter-haskell/app/Main.hs
+++ remember-the-letter-haskell/app/Main.hs
@@ -62,12 +62,12 @@ commandLineLoop flashcards =
        "add"  ->
               do
               putStrLn "Adding a flashcard..."
-              putStr "Enter the front side "
+              putStrLn "Enter the front side"
               printPrompt
               fSide <- getLine
               putStrLn ("You entered the following for the front side: (" ++
                          fSide ++ ")")
-              putStr "Enter the back side "
+              putStrLn "Enter the back side"
               printPrompt
               bSide  <- getLine
               putStrLn ("You entered the following for the back side: (" ++
