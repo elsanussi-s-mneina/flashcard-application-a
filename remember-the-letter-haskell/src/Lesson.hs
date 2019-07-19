aa6e9b4fa584e6d54607ac66fab9100b998b0e43
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2019-07-19 14:39:32 -0400

    Correcting a typo

--- remember-the-letter-haskell/src/Lesson.hs
+++ remember-the-letter-haskell/src/Lesson.hs
@@ -6,7 +6,7 @@ Description : A lesson consists of flashcard data, and related functions.
 
 module Lesson where
 
--- | A flahscard has a front side, and a back side.
+-- | A flashcard has a front side, and a back side.
 data Flashcard =
   Flashcard
   { front :: String
