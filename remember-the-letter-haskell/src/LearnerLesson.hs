807d1dd5d3e09d0f404a54b6263d9021fc47d663
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-06-24 13:41:09 -0400

    I documented all non-test functions and parameters.

--- remember-the-letter-haskell/src/LearnerLesson.hs
+++ remember-the-letter-haskell/src/LearnerLesson.hs
@@ -6,18 +6,24 @@ module LearnerLesson where
 
 import Lesson(Flashcard(Flashcard), back, front)
 
--- A flashcard with user specific information.
+-- | A flashcard with user specific information.
 data LearnerFlashcard = LearnerFlashcard
-  { flashcard :: Flashcard
-  , correctCount :: Int
-  , attemptCount :: Int
+  { flashcard :: Flashcard   -- ^ a flashcard
+  , correctCount :: Int      -- ^ the number of correct guesses the user made
+  , attemptCount :: Int      -- ^ the number of guesses both correct or incorrect the user made
   } 
   deriving (Show, Eq)
 
-learnerAnswersCorrectly :: LearnerFlashcard -> LearnerFlashcard
+-- | The function that given a flashcard (and learner-flashcard statistics) returns the flashcard
+-- | after its learner recalls that flashcard correctly.
+learnerAnswersCorrectly :: LearnerFlashcard -- ^ a flashcard with statistics
+                        -> LearnerFlashcard -- ^ the flashcard with statistics after a correct answer
 learnerAnswersCorrectly (LearnerFlashcard {flashcard = f, correctCount = cc, attemptCount = ac}) = 
   LearnerFlashcard {flashcard =  f, correctCount = 1 + cc, attemptCount = 1 + ac}
 
-learnerAnswersIncorrectly :: LearnerFlashcard -> LearnerFlashcard
+-- | The function that given a flashcard (and learner-flashcard statistics) returns the flashcard
+-- | after its learner fails to recall that flashcard.
+learnerAnswersIncorrectly :: LearnerFlashcard -- ^ a flashcard with statistics
+                          -> LearnerFlashcard -- ^ the flashcard with statistics after an incorrect answer
 learnerAnswersIncorrectly (LearnerFlashcard {flashcard = f, correctCount = cc, attemptCount = ac}) = 
   LearnerFlashcard {flashcard =  f, correctCount = cc, attemptCount = ac + 1}
--- remember-the-letter-haskell/src/Lesson.hs
+++ remember-the-letter-haskell/src/Lesson.hs
@@ -15,14 +15,18 @@ data Flashcard =
   } deriving (Eq, Show)
 
 
--- | Add a flashcard to a lesson.
--- | Works by returning the lesson with the new flashcard added.
-addFlashcardToLesson :: [Flashcard] -> String -> String -> [Flashcard]
+-- | This function adds a flashcard to a lesson.
+-- This function works by returning the lesson with the new flashcard added.
+addFlashcardToLesson :: [Flashcard] -- ^ a list of flashcards
+                     -> String      -- ^ front side of new flashcard
+                     -> String      -- ^ back side of new flashcard
+                     -> [Flashcard] -- ^ a list of flashcards with a new flashcard
 addFlashcardToLesson flashcards front back =
   flashcards ++ [(Flashcard front back)]
 
 -- | The front and backs of every flashcard in a given list.
-lessonSummary :: [Flashcard] -> String
+lessonSummary :: [Flashcard] -- ^ a list of flashcards
+              -> String      -- ^ text containing front and back of each flashcard
 lessonSummary flashcards = unlines (map showFlashcard flashcards)
 
 -- | The front of every flashcard in a given list.
@@ -38,23 +42,32 @@ backSummary :: [Flashcard]   -- ^ list of flashcards
 backSummary flashcards = unlines (map showFlashcardBack flashcards)
 
 -- | The front and back of a single flashcard.
-showFlashcard :: Flashcard -> String
+showFlashcard :: Flashcard -- ^ a flashcard
+              -> String    -- ^ text contained on the front and back of the flashcard
 showFlashcard card = front card ++ " | " ++ back card
 
 -- | Show the front of a single flashcard.
-showFlashcardFront :: Flashcard -> String
+showFlashcardFront :: Flashcard -- ^ a flashcard
+                   -> String    -- ^ text contained on the front of the flashcard
 showFlashcardFront = front
 
 -- | Show the back of a single flashcard.
-showFlashcardBack :: Flashcard -> String
+showFlashcardBack :: Flashcard -- ^ a flashcard
+                  -> String    -- ^ text contained on the back of the flashcard
 showFlashcardBack = back
 
 -- | Convert a flashcard to tab separated values.
-tabSeparatedValuesOfFlashcard :: Flashcard -> String
+tabSeparatedValuesOfFlashcard :: Flashcard -- ^ a flashcard
+                              -> String    -- ^ a line of text containing the front of the flashcard, then
+                                           --   a tab character, then the back of the flashcard, and then
+                                           --   a new line character.   
 tabSeparatedValuesOfFlashcard (Flashcard f b) = f ++ "\t" ++ b ++ "\n"
 
 -- | Convert tab separated values (a single line) to a flashcard.
-tabSeparatedValuesToFlashcard :: String -> Flashcard
+tabSeparatedValuesToFlashcard :: String -- ^ a line of text containing the front of a flashcard,     
+                                        --   then a tab character, then the back of the flashcard,
+                                        --   then a new line character.
+                              -> Flashcard -- ^ a flashcard
 tabSeparatedValuesToFlashcard line =
   let line' = filter (/= '\n') line
       (front, backWithTabAtFront) = break (=='\t') line'
@@ -64,27 +77,40 @@ tabSeparatedValuesToFlashcard line =
 
 
 -- | Convert the flashcards to a tab separated values format.
-tabSeparatedValuesOfLesson :: [Flashcard] -> String
+tabSeparatedValuesOfLesson :: [Flashcard] -- ^  list of flashcards
+                           -> String      -- ^  plain text containing details of every flashcard
+                                          --    on a separate line, in tab separated values format.  
 tabSeparatedValuesOfLesson =
   concatMap tabSeparatedValuesOfFlashcard
 
 -- | Convert tab separated values (multiple lines) to flashcards.
-tabSeparatedValuesToLesson :: String -> [Flashcard]
+tabSeparatedValuesToLesson :: String      -- ^ plain text containing details of every flashcard on a separate line,
+--                                        --   in tab separated values format.
+                           -> [Flashcard] -- ^ a list of flashcards
 tabSeparatedValuesToLesson contents =
   map tabSeparatedValuesToFlashcard (lines contents)
 
 -- | surrounding a string with double quotes
-doubleQuote :: String -> String
+doubleQuote :: String -- ^ some text
+            -> String -- ^ the same text but with two extra characters surrounding it, both double quote characters.
 doubleQuote s = "\"" ++ s ++ "\""
 
-presentBackOfFlashcard :: Flashcard -> String
+presentBackOfFlashcard :: Flashcard -- ^ a flashcard
+                       -> String    -- ^ text declaring the back of a flashcard to the user,
+                                    --   in a clear manner.  
 presentBackOfFlashcard fla = "I am showing you the back of a flashcard.\nYou see " ++ doubleQuote (showFlashcardBack fla)
 
-presentFrontOfFlashcard :: Flashcard -> String
+presentFrontOfFlashcard :: Flashcard -- ^ a flashcard
+                        -> String    -- ^ text declaring the front of a flashcard to the user,
+                                     --   in a clear manner.     
 presentFrontOfFlashcard fla = "I am showing you the front of a flashcard.\nYou see " ++ doubleQuote (showFlashcardFront fla)
 
-checkUserAttemptToProvideBack :: Flashcard -> String -> Bool
+checkUserAttemptToProvideBack :: Flashcard -- ^ a flashcard
+                              -> String    -- ^ user's guess of what the back of the flashcard is
+                              -> Bool      -- ^ whether the guess is correct
 checkUserAttemptToProvideBack fla userAttempt = userAttempt == back fla
 
-checkUserAttemptToProvideFront :: Flashcard -> String -> Bool
+checkUserAttemptToProvideFront :: Flashcard -- ^ a flashcard
+                               -> String    -- ^ user's guess of what the front of the flashcard is
+                               -> Bool      -- ^ whether the guess is correct
 checkUserAttemptToProvideFront fla userAttempt = userAttempt == front fla
